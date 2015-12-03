9c69647f980b13dabc71dfef2aa675a5bfb97732
Giles Bowkett <gilesb@gmail.com>
2015-12-02 21:47:15 -0700

    updated clojure, but on old overtone
    meh. using new overtone, I get weird performance fails.
    https://github.com/technomancy/leiningen/pull/1230 seems related,
    but did not fix.

--- project.clj
+++ project.clj
@@ -4,6 +4,6 @@
   :url "http://gilesb.com/arx" ; no actual site yet >.<
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [overtone "0.8.1"]])
 
